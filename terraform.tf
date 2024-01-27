23ee646e468640241763156b1ef30c65af576dbc
nikhil <anc@gmail.com>
2024-01-27 12:33:48 +0530

    all

--- terraform.tf
+++ terraform.tf
@@ -2,27 +2,25 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eks-spot-cluster"
   cluster_version = "1.20"
-  subnets         = ["subnet-0eff86e19581e95ec", "subnet-0b062e7252a2101ca"]
-  vpc_id          = "vpc-021a2ac87501570e4"
+
+  vpc_config = {
+    subnets = ["subnet-0eff86e19581e95ec", "subnet-0b062e7252a2101ca"] # replace with your subnet ids
+  }
+
+  node_groups_defaults = {
+    instance_type = "t3.medium"
+    key_name      = "chandra"
+    additional_tags = {
+      Environment = "test"
+      Name        = "eks-spot-cluster"
+    }
+  }
 
   node_groups = {
     eks_nodes = {
       desired_capacity = 3
       max_capacity     = 5
       min_capacity     = 1
-
-      instance_type = "t3.medium"
-      key_name      = "chandra"
-
-      k8s_labels = {
-        Environment = "test"
-        Name        = "eks-spot-cluster"
-      }
-
-      additional_tags = {
-        Environment = "test"
-        Name        = "eks-spot-cluster"
-      }
     }
   }
 }
