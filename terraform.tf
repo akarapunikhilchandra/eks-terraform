0c2dadd8703772f9caeb485588e698624e2346d5
nikhil <anc@gmail.com>
2024-01-27 12:45:01 +0530

    all

--- terraform.tf
+++ terraform.tf
@@ -1,26 +1,119 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
   cluster_name    = "eks-spot-cluster"
-  cluster_version = "1.20"
+  cluster_version = "1.27"
 
-  vpc_config = {
-    subnets = ["subnet-0eff86e19581e95ec", "subnet-0b062e7252a2101ca"] # replace with your subnet ids
-  }
+  cluster_endpoint_public_access  = true
 
-  node_groups_defaults = {
-    instance_type = "t3.medium"
-    key_name      = "chandra"
-    additional_tags = {
-      Environment = "test"
-      Name        = "eks-spot-cluster"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 3
-      max_capacity     = 5
-      min_capacity     = 1
+  vpc_id                   = "vpc-021a2ac87501570e4"
+  subnet_ids               = ["subnet-0eff86e19581e95ec", "subnet-0eff86e19581e95ec"]
+  control_plane_subnet_ids = ["subnet-0eff86e19581e95ec", "subnet-0eff86e19581e95ec"]
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "t3.medium"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     }
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 3
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "t3.medium"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "t3.medium"
+            weighted_capacity = "2"
+          },
+        ]
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::767398108107:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::767398108107:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  aws_auth_accounts = [
+    "767398108107",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
