6bbfab0c9bf3a0200a09300cbc6efc28ca1a5916
nikhil <anc@gmail.com>
2024-01-27 11:43:29 +0530

    all

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,34 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "eks-spot-cluster"
-  subnets         = ["subnet-0eff86e19581e95ec"]
-  vpc_id          = "vpc-021a2ac87501570e4"
-  cluster_version = "1.21"  # Set your desired Kubernetes version
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks-spot-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.21"  # Set your desired Kubernetes version
 
-  node_groups = {
-    spot = {
-      desired_capacity = 3
-      max_capacity     = 5
-      min_capacity     = 1
-
-      key_name = "chandra"  # Replace this with your keypair exists in AWS
-      instance_type = "t3.medium"
-    }
+  vpc_config {
+    subnet_ids = ["subnet-0eff86e19581e95ec"]
   }
+}
+
+resource "aws_eks_node_group" "spot_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "spot"
+
+  node_role_arn = aws_iam_role.eks_node_group.arn
+  subnet_ids    = ["subnet-0eff86e19581e95ec"]
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 5
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+  key_name       = "chandra"  # Replace this with your keypair exists in AWS
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks_cluster"
+}
+
+resource "aws_iam_role" "eks_node_group" {
+  name = "eks_node_group"
 }
