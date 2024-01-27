2b9a056bec87c092346f07eb0f6d215f857e40a4
nikhil <anc@gmail.com>
2024-01-27 13:39:40 +0530

    all

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "this"
+  cluster_name    = "eks-spot-cluster"
   cluster_version = "1.27"
 
   cluster_endpoint_public_access  = true
@@ -23,52 +23,13 @@ module "eks" {
   subnet_ids               = ["subnet-0eff86e19581e95ec", "subnet-0b062e7252a2101ca"]  # Replace with correct subnet ID
   control_plane_subnet_ids = ["subnet-0eff86e19581e95ec", "subnet-0b062e7252a2101ca"]  # Replace with correct subnet ID
 
-  # Self Managed Node Group(s)
-  self_managed_node_group_defaults = {
-    instance_type                          = "t3.medium"
-    update_launch_template_default_version = true
-    iam_role_additional_policies = {
-      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-    }
-  }
-
-  self_managed_node_groups = {
-    one = {
-      name         = "mixed-1"
-      max_size     = 5
-      desired_size = 2
-      azs          = ["us-east-1a", "us-east-1d"]
-
-      use_mixed_instances_policy = true
-      mixed_instances_policy = {
-        instances_distribution = {
-          on_demand_base_capacity                  = 0
-          on_demand_percentage_above_base_capacity = 3
-          spot_allocation_strategy                 = "capacity-optimized"
-        }
-
-        override = [
-          {
-            instance_type     = "t3.medium"
-            weighted_capacity = "1"
-          },
-          {
-            instance_type     = "t3.medium"
-            weighted_capacity = "2"
-          },
-        ]
-      }
-    }
-  }
-
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]
   }
 
   eks_managed_node_groups = {
-    blue = {}
-    green = {
+    default = {
       min_size     = 1
       max_size     = 3
       desired_size = 1
@@ -78,41 +39,6 @@ module "eks" {
       capacity_type  = "SPOT"
     }
   }
-
-  # Fargate Profile(s)
-  fargate_profiles = {
-    default = {
-      name = "default"
-      selectors = [
-        {
-          namespace = "default"
-        }
-      ]
-    }
-  }
-
-  # aws-auth configmap
-  manage_aws_auth_configmap = true
-
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::767398108107:role/role1"
-      username = "role1"
-      groups   = ["system:masters"]
-    },
-  ]
-
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::767398108107:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    }
-  ]
-
-  aws_auth_accounts = [
-    "767398108107",
-  ]
 
   tags = {
     Environment = "dev"
