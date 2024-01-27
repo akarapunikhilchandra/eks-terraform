f2ce823ca11efb16c78630adc21b8dbfa353b112
nikhil <anc@gmail.com>
2024-01-27 10:56:37 +0530

    all

--- provider.tf
+++ provider.tf
@@ -5,5 +5,15 @@ terraform {
       version = "5.33.0"
     }
   }
+  backend "s3" {
+    bucket = "roboshop-locking" 
+    key = "eks-terraform" 
+    region = "us-east-1"
+    dynamodb_table = "roboshop-state-lock" 
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
 }
 
